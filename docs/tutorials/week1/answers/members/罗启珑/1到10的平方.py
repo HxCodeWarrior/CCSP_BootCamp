c989be37bdf501b14c15d85462cba02023e580bd
HxCodeWarrior <296946361@qq.com>
2024-09-23 23:53:01 +0800

    第一次考核结果

---
+++ docs/tutorials/week1/answers/members/罗启珑/1到10的平方.py
@@ -0,0 +1,3 @@
+for i in range(1, 11):
+    square = i * i
+    print(f"{i} 的平方是 {square}")
---
+++ docs/tutorials/week1/answers/members/罗启珑/判断奇数偶数.py
@@ -0,0 +1,7 @@
+num = int(input("请输入一个整数: "))
+
+# 判断是奇数还是偶数
+if num % 2 == 0:
+    print(f"{num} 是偶数")
+else:
+    print(f"{num} 是奇数")
